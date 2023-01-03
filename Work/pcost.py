e8df1c141504dc62a95f2370114de2ef5d1a9893
Alex Forde <alex.forde@evcargo.com>
2023-01-03 14:39:27 +0000

    Updated pcost.py for Exercise 1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,9 +2,18 @@
 #
 # Exercise 1.27
 
-total = 0
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f).split(',')
-    for line in f:
-        total += float(line.split(',')[-1])
-print(f'Total cost {round(total,2)}')
+def portfolio_cost(filename):
+    total = 0
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        for line in f:
+            fields = line.split(',')
+            try:
+                shares = int(fields[1])
+                price = float(fields[2])
+                total += (shares * price)
+            except ValueError:
+                print(f'line {line} cannot be parsed for a share and/or price')
+    print(f'Total cost {round(total,2)}')
+
+#portfolio_cost('Data/portfolio.csv')
