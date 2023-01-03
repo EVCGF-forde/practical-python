1dcbfd4d2bcf21415df6df6c8524f0f8339c0d5e
Alex Forde <alex.forde@evcargo.com>
2023-01-03 13:42:40 +0000

    Completed Exercise 1.27 pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total = 0
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f).split(',')
+    for line in f:
+        total += float(line.split(',')[-1])
+print(f'Total cost {round(total,2)}')
