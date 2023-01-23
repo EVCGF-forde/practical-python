92719e030833f19382a5ba50309149e07df32eb4
Alex Forde <alex.forde@evcargo.com>
2023-01-23 13:00:13 +0000

    Completed Exercise 3.7 although haven't included error checking and restrictions of parameters that can't be used together

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,20 +3,26 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, *, select=None):
+def parse_csv(filename, *, select=None, types=None, has_headers=True, delimiter=','):
     '''Parse a CSV file into a list of records'''
     with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        indices = None if not select else [c for c, v in enumerate(headers) if v in select]
-        if indices:
-            headers = [headers[i] for i in indices]
+        rows = csv.reader(f, delimiter=delimiter)
+        if has_headers:
+            headers = next(rows)
+            indices = None if not select else [c for c, v in enumerate(headers) if v in select]
+            if indices:
+                headers = [headers[i] for i in indices]
         records = []
         for row in rows:
             if not row:
                 continue
-            if indices:
+            if has_headers and indices:
                 row = [row[i] for i in indices]
-            record = dict(zip(headers, row))
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record =tuple(row)
             records.append(record)
     return records
