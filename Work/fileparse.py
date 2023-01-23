a7fdd9e667465abc0851eb6c046fa751b11818a8
Alex Forde <alex.forde@evcargo.com>
2023-01-23 15:36:38 +0000

    Completed Exercise 3.8 Raising exceptions

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,8 +5,11 @@ import csv
 
 def parse_csv(filename, *, select=None, types=None, has_headers=True, delimiter=','):
     '''Parse a CSV file into a list of records'''
+    if select and not has_headers:
+        raise RuntimeError("Select argument requires column headers")
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
+        indices = None
         if has_headers:
             headers = next(rows)
             indices = None if not select else [c for c, v in enumerate(headers) if v in select]
@@ -16,13 +19,13 @@ def parse_csv(filename, *, select=None, types=None, has_headers=True, delimiter=
         for row in rows:
             if not row:
                 continue
-            if has_headers and indices:
+            if indices:
                 row = [row[i] for i in indices]
             if types:
                 row = [func(val) for func, val in zip(types, row)]
             if has_headers:
                 record = dict(zip(headers, row))
             else:
-                record =tuple(row)
+                record = tuple(row)
             records.append(record)
     return records
